67baa7bc104872048221ccfdae501b1fc3b5ac40
Albert <toroxtyne7@gmail.com>
2023-09-12 21:50:07 +0600

    big button

--- app/src/main/java/com/example/mypr42mviver3/BirdViewModel.kt
+++ app/src/main/java/com/example/mypr42mviver3/BirdViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import androidx.room.Room
 import com.example.mypr42mviver3.data.BirdDao
 import com.example.mypr42mviver3.data.BirdEntity
+import com.example.mypr42mviver3.data.BirdIntent
 import com.example.mypr42mviver3.data.BirdViewStates
 import com.example.mypr42mviver3.data.BirdsRepo
 import com.example.mypr42mviver3.data.MyDB
@@ -13,7 +14,6 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-
 class BirdViewModel : ViewModel() {
     private val birdDB: MyDB =
         Room.databaseBuilder(App.instance.applicationContext, MyDB::class.java, name = "BDB")
@@ -24,7 +24,19 @@ class BirdViewModel : ViewModel() {
     private val _viewState = MutableStateFlow<BirdViewStates>(BirdViewStates.Loading)
     val viewState: StateFlow<BirdViewStates> = _viewState.asStateFlow()
 
-    fun loadBirds() {
+    init {
+        processIntent(BirdIntent.LoadBirds)
+    }
+
+    fun processIntent(intent: BirdIntent) {
+        when (intent) {
+            is BirdIntent.AddBird -> insertBirds(intent.bird)
+            is BirdIntent.RemoveBird -> deleteBirds(intent.bird)
+            is BirdIntent.LoadBirds -> loadBirds()
+        }
+    }
+
+    private fun loadBirds() {
         _viewState.value = BirdViewStates.Loading
         viewModelScope.launch {
             try {
@@ -36,13 +48,13 @@ class BirdViewModel : ViewModel() {
         }
     }
 
-    fun insertBirds(bird: BirdEntity) {
+    private fun insertBirds(bird: BirdEntity) {
         viewModelScope.launch {
             repo.insertBird(bird)
         }
     }
 
-    fun deleteBirds(bird: BirdEntity) {
+    private fun deleteBirds(bird: BirdEntity) {
         viewModelScope.launch {
             repo.deleteBird(bird)
         }
--- app/src/main/java/com/example/mypr42mviver3/MainActivity.kt
+++ app/src/main/java/com/example/mypr42mviver3/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.mypr42mviver3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,6 +36,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mypr42mviver3.data.BirdEntity
+import com.example.mypr42mviver3.data.BirdIntent
 import com.example.mypr42mviver3.data.BirdViewStates
 import com.example.mypr42mviver3.ui.theme.MyPr42MVIVer3Theme
 
@@ -43,17 +57,25 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting(vm: BirdViewModel = viewModel()) {
     var viewStates by remember { mutableStateOf<BirdViewStates>(BirdViewStates.Loading) }
+    var isAddingBird by remember { mutableStateOf(false) }
+    var newBirdName by remember { mutableStateOf("") }
+    var newBirdAge by remember { mutableStateOf("") }
 
+    // Отправляем интент для загрузки данных
     LaunchedEffect(Unit) {
+        vm.processIntent(BirdIntent.LoadBirds)
 
-        vm.loadBirds()
+
+        // Наблюдаем за изменениями состояния во ViewModel
         vm.viewState.collect { ns ->
             viewStates = ns
         }
     }
+
     when (viewStates) {
         is BirdViewStates.Loading -> {
             // Отобразить индикатор загрузки
@@ -69,7 +91,30 @@ fun Greeting(vm: BirdViewModel = viewModel()) {
             // Отобразить список с данными
             LazyColumn {
                 items((viewStates as BirdViewStates.Success).birds) { bird ->
-                    Text(text = bird.toString(), modifier = )
+                    Row(
+                        modifier = Modifier.clickable {
+                            // Создайте интент для удаления птицы и отправьте его во ViewModel
+                            val removeIntent = BirdIntent.RemoveBird(bird)
+                            vm.processIntent(removeIntent)
+                        },
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(text = bird.toString())
+                        Spacer(modifier = Modifier.width(8.dp))
+                        // Добавьте кнопку или иконку для удаления элемента
+                        IconButton(
+                            onClick = {
+                                // Создайте интент для удаления птицы и отправьте его во ViewModel
+                                val removeIntent = BirdIntent.RemoveBird(bird)
+                                vm.processIntent(removeIntent)
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Delete,
+                                contentDescription = "Delete"
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -81,5 +126,51 @@ fun Greeting(vm: BirdViewModel = viewModel()) {
                 modifier = Modifier.padding(16.dp)
             )
         }
+    }
+
+    // Показать форму для добавления элемента
+    if (isAddingBird) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            OutlinedTextField(
+                value = newBirdName,
+                onValueChange = { newBirdName = it },
+                label = { Text("Name") }
+            )
+            OutlinedTextField(
+                value = newBirdAge,
+                onValueChange = { newBirdAge = it },
+                label = { Text("Age") }
+            )
+            Button(
+                onClick = {
+                    // Создать интент для добавления новой птицы и отправить его во ViewModel
+                    val addIntent = BirdIntent.AddBird(
+                        BirdEntity(
+                            name = newBirdName,
+                            age = newBirdAge.toIntOrNull() ?: 0
+                        )
+                    )
+                    vm.processIntent(addIntent)
+
+                    // Сбросить состояние формы
+                    isAddingBird = false
+                    newBirdName = ""
+                    newBirdAge = ""
+                },
+                modifier = Modifier.padding(16.dp).fillMaxWidth()
+            ) {
+                Text("Add Bird")
+            }
+        }
+    }
+
+    // Кнопка для отображения формы для добавления элемента
+    Button(
+        onClick = {
+            isAddingBird = true
+        },
+        modifier = Modifier.padding(16.dp).fillMaxWidth()
+    ) {
+        Text("Add Bird")
     }
 }
